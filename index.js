adc78e96a0b9e9762a696bcb37723e73533f9f00
Rohit Hazra <talk2rohithzr@gmail.com>
2016-08-04 04:58:43 +0530

    Comment Migration Done

--- index.js
+++ index.js
@@ -37,8 +37,12 @@ var gitlab_new = gitlab({
 for(var i = 0; i < source.projects.length; i++){
     startMigration(i);
 }
-
+//
+//console.log(gitlab_old.projects.issues.notes);
+//
 function startMigration(i){
+	var j = 0;
+	var k = 0;
   	// Get Project
 	gitlab_old.projects.show(source.projects[i], function(sourceProject) {
 	  	console.log("source project found : " + sourceProject.path_with_namespace);
@@ -51,18 +55,35 @@ function startMigration(i){
 	  				console.log("source issues found");
 
 		  			gitlab_new.projects.issues.list(destinationProject.id, function(destinationIssues) {
-		  				if(destinationIssues.length != sourceIssues.length) {
+		  				//if(destinationIssues.length != sourceIssues.length) {
+		  				if(true) {
 			  				console.log("creating issues");
 
-						    function createIssue(i){
-						    	if(i < sourceIssues.length){
-						    		gitlab_new.issues.create(destinationProject.id, sourceIssues[i], function(response, err){
-										console.log(response);
-										createIssue(i+1);
+			  				
+						    function createIssue(j){
+						    	if(j < sourceIssues.length){
+						    		gitlab_new.issues.create(destinationProject.id, sourceIssues[j], function(response, err){
+						    			console.log("creating issue #"+j);
+										if(sourceIssues[j].user_notes_count > destinationIssues[j].user_notes_count){
+											console.log("get comments");
+
+											gitlab_old.projects.issues.notes.all(sourceProject.id, sourceIssues[j].id, function(sourceComments, err){
+												function createComment(k){
+						    						if(k < sourceComments.length){														
+														gitlab_new.notes.create(destinationProject.id, destinationIssues[j].id, sourceComments[k], function(response, err){
+															console.log("create comment");
+
+															createComment(k+1);
+														});
+						    						}
+						    					}
+						    					createComment(0);
+											});
+										}
+										createIssue(j+1);
 									});
 						    	}
 							}
-
 							createIssue(0);
 		  				}else{
 			  				console.log("issues already copied");
@@ -72,15 +93,4 @@ function startMigration(i){
 			});	  		
 	  	});
 	});
-}
-
-function createIssue(projectId, sourceIssues, callback){
-	for(var j = 0; j < sourceIssues.length; j++){
-		gitlab_new.issues.create(projectId, sourceIssues[i], function(response, err){
-			if(err){
-				callback(err);
-			}
-			console.log(response);
-		});
-	}
 }
